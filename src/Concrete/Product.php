682265df8c5951fea8c72a02d91d1100ecd33f9b
Lukasz Grajewski <grajewski.lukasz@wp.pl>
2021-05-23 21:05:05 +0200

    Add implementation for product contract

--- src/Concrete/Product.php
+++ src/Concrete/Product.php
@@ -1,9 +1,39 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Laracart\Concrete;
 
-class Product
-{
+use Money\Money;
+use JetBrains\PhpStorm\Immutable;
 
+#[Immutable]
+class Product implements \Laracart\Contracts\Product
+{
+    public function __construct(
+        private mixed $id,
+        private string $name,
+        private Money $price,
+        private int $quantity
+    ) {}
+
+    public function getId(): mixed
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPrice(): Money
+    {
+        return $this->price;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
 }
