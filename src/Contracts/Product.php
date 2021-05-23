fc3ac4b89419d1ec7680837808cc925fa5bffa7c
Lukasz Grajewski <grajewski.lukasz@wp.pl>
2021-05-23 17:12:13 +0200

    Add package contracts

--- src/Contracts/Product.php
+++ src/Contracts/Product.php
@@ -1,10 +1,15 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Laracart\Contracts;
 
+use Money\Money;
 
 interface Product
 {
-
+    public function getId(): mixed;
+    public function getName(): string;
+    public function getPrice(): Money;
+    public function getQuantity(): int;
 }
