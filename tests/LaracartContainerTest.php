71aaec8c473e4b2dd12b9d6c6c28dec0c073456e
Lukasz Grajewski <grajewski.lukasz@wp.pl>
2021-05-24 20:42:38 +0200

    Add tests'

---
+++ tests/LaracartContainerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laracart\Tests;
+
+use Mockery\Mock;
+use Laracart\Laracart;
+use Laracart\Contracts\Product;
+use Laracart\Providers\ServiceProvider;
+
+class LaracartContainerTest extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [ServiceProvider::class];
+    }
+
+    /**
+     *
+     * @covers
+     */
+    public function testSingletonInstance()
+    {
+        $instance = app('laracart');
+        $this->assertInstanceOf(Laracart::class, $instance);
+    }
+
+    /**
+     *
+     * @covers
+     */
+    public function testFacade()
+    {
+        $productMock = \Mockery::mock(Product::class, []);
+        $product = \Laracart\Facades\Laracart::add($productMock);
+        $this->assertSame($productMock, $product);
+        $this->assertCount(1, \Laracart\Facades\Laracart::items());
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,13 @@
 
 declare(strict_types=1);
 
+namespace Laracart\Tests;
+
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        \Mockery::close();
+    }
 }
