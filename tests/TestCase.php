689d73d3771bbcd21ce11c63ee7d3bfe60d4dcd9
Lukasz Grajewski <grajewski.lukasz@wp.pl>
2021-05-26 19:59:14 +0200

    Update testcase, add package providers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace Laracart\Tests;
 
+use Laracart\Providers\ServiceProvider;
+
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    public function getPackageProviders($app): array
+    {
+        return [
+            ServiceProvider::class
+        ];
+    }
+
     public function tearDown(): void
     {
         parent::tearDown();
