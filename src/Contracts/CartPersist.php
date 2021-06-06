6c303474d8fe51897b7a2f1b976aa7166cca7edf
Lukasz Grajewski <grajewski.lukasz@wp.pl>
2021-06-06 12:50:07 +0200

    Update contracts

--- src/Contracts/CartPersist.php
+++ src/Contracts/CartPersist.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Laracart\Contracts;
 
+use Illuminate\Support\Collection;
 use Laracart\Exceptions\CartPersistException;
 
-interface Storable
+interface CartPersist
 {
     /**
+     * @param Collection<\Laracart\Concrete\Product> $products
      * @return bool
      * @throws CartPersistException
      */
-    public function save(): bool;
+    public function save(Collection $products): bool;
 }
