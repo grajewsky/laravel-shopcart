825ed77f9b81c00c0ba6b83d67e0d1a5e41e773a
Lukasz Grajewski <grajewski.lukasz@wp.pl>
2021-06-07 22:10:10 +0200

    Update service provider

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -5,16 +5,20 @@ declare(strict_types=1);
 namespace Laracart\Providers;
 
 use Laracart\Laracart;
+use Laracart\Services\Persist\Eloquent;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . "/../../config/config.php", "laracart");
+        $this->app->singleton(Eloquent::class, fn($app) => new Eloquent(
+            config('laracart', []),
+        );
 
         $this->app->singleton("laracart", fn($app) => new Laracart(
             config('laracart', []),
-            $this->app['session'],
+            $this->app['session']),
             $this->app['events']
         ));
     }
