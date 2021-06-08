8fac3d39ee71299dd89559a8dd7c8e18c10074f3
Lukasz Grajewski <grajewski.lukasz@wp.pl>
2021-06-08 19:42:35 +0200

    Implements eloquent stored driver

--- src/Services/Persist/Eloquent.php
+++ src/Services/Persist/Eloquent.php
@@ -4,17 +4,21 @@ declare(strict_types=1);
 
 namespace Laracart\Services\Persist;
 
+use Laracart\Events\CartStored;
 use Illuminate\Support\Collection;
 use JetBrains\PhpStorm\ArrayShape;
+use Illuminate\Support\Facades\DB;
 use Laracart\Contracts\CartPersist;
 use Laracart\Shapes\ConfigurationShape;
-use Laracart\Exceptions\CartPersistException;
+use Laracart\Exceptions\NotFoundException;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class Eloquent implements CartPersist
 {
     public function __construct(
         #[ArrayShape(ConfigurationShape::SHAPE)]
         private array $configuration,
+        private EventDispatcher $dispatcher
     ) {}
 
 
@@ -25,6 +29,32 @@ class Eloquent implements CartPersist
      */
     public function store(string $identifier, Collection $products): bool
     {
-        return false;
+        DB::table($this->configuration['eloquent']['table'])
+            ->insert([
+                'id' => $identifier,
+                'items' => serialize($products)
+            ]);
+
+        $this->dispatcher->dispatch(new CartStored($products));
+    }
+
+    /**
+     * @param string $identifier
+     * @return Collection
+     * @throws NotFoundException
+     */
+    public function restore(string $identifier): Collection
+    {
+        $laracart = DB::table($this->configuration['eloquent']['table'])
+            ->select(['items'])
+            ->first(['items']);
+
+        if (is_null($laracart)) {
+            throw new NotFoundException('Laracart with id: ' . $identifier . ' not found.');
+        }
+
+        $restored = unserialize($laracart->items);
+
+        return $restored;
     }
 }
