bbbd5d850ef605d1f46c690fdcae4f106c8b813f
Lukasz Grajewski <grajewski.lukasz@wp.pl>
2021-06-08 19:24:25 +0200

    Implements event class

--- src/Events/CartStored.php
+++ src/Events/CartStored.php
@@ -1,9 +1,20 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Laracart\Events;
 
+use Illuminate\Support\Collection;
+use Illuminate\Queue\SerializesModels;
+
 class CartStored
 {
+    use SerializesModels;
 
+    public function __construct(private Collection $products) { }
+
+    public function getItems(): Collection
+    {
+        return $this->products;
+    }
 }
